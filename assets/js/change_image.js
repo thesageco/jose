7e35bd02608ec2100825aba9d64c7f745923e023
Aditya Arora <10962267+arora-aditya@users.noreply.github.com>
2018-12-28 17:54:16 -0500

    add prefetch logic and gallery view - pending font details from emile

---
+++ assets/js/change_image.js
@@ -0,0 +1,16 @@
+const MAX_IMAGES = 6;
+var current_IMAGE = 1;
+
+function changeImage(){
+  document.getElementById("changing").src="../assets/images/" + String(current_IMAGE) + ".jpg";
+  current_IMAGE = (current_IMAGE+1)%MAX_IMAGES;
+}
+
+function loadImages(){
+for(var i = 0; i < MAX_IMAGES; i+=1){
+  var Image = document.createElement("img")
+  Image.src = "../assets/images/" + String(i) + ".jpg"
+  Image.className = "d-none"
+  document.body.appendChild(Image);
+}
+}
