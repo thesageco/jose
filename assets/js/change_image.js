e0088ad071ec97be9a250b61e171eebe24beead9
Aditya Arora <10962267+arora-aditya@users.noreply.github.com>
2018-12-29 18:09:54 -0500

    remoe onload

--- assets/js/change_image.js
+++ assets/js/change_image.js
@@ -1,12 +1,7 @@
 const MAX_IMAGES = 6;
 var current_IMAGE = 1;
 
-function changeImage(){
-  document.getElementById("changing").src="../assets/images/" + String(current_IMAGE) + ".jpg";
-  current_IMAGE = (current_IMAGE+1)%MAX_IMAGES;
-}
-
-function loadImages(){
+// pre load all the images
 for(var i = 0; i < MAX_IMAGES; i+=1){
   var Image = document.createElement("img")
   Image.src = "../assets/images/" + String(i) + ".jpg";
@@ -15,4 +10,8 @@ for(var i = 0; i < MAX_IMAGES; i+=1){
   Image.height = 0;
   document.body.appendChild(Image);
 }
+
+function changeImage(){
+  document.getElementById("changing").src="../assets/images/" + String(current_IMAGE) + ".jpg";
+  current_IMAGE = (current_IMAGE+1)%MAX_IMAGES;
 }
