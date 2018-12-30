7157667373078f644e0d5b6912a40ca858226760
Aditya Arora <10962267+arora-aditya@users.noreply.github.com>
2018-12-30 18:33:14 -0500

    add instant-fetch, fix onLoad error

--- assets/js/change_image.js
+++ assets/js/change_image.js
@@ -1,17 +1,19 @@
 const MAX_IMAGES = 6;
-var current_IMAGE = 1;
+var current_IMAGE = 0;
 
-// pre load all the images
-for(var i = 0; i < MAX_IMAGES; i+=1){
-  var Image = document.createElement("img")
-  Image.src = "../assets/images/" + String(i) + ".jpg";
-  // Image.className = "d-none"
-  Image.width = 0;
-  Image.height = 0;
-  document.body.appendChild(Image);
+function prefetch(){
+  // pre load all the images
+  for(var i = current_IMAGE + 1; i < MAX_IMAGES; i+=1){
+    var Image = document.createElement("img")
+    Image.src = "../assets/images/" + String(i) + ".jpg";
+    // Image.className = "d-none"
+    Image.width = 0;
+    Image.height = 0;
+    document.body.appendChild(Image);
+  }
 }
 
 function changeImage(){
-  document.getElementById("changing").src="../assets/images/" + String(current_IMAGE) + ".jpg";
   current_IMAGE = (current_IMAGE+1)%MAX_IMAGES;
+  document.getElementById("changing").src="../assets/images/" + String(current_IMAGE) + ".jpg";
 }
