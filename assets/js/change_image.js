120876ab8b1c1f951f172b9799469f8e7a305942
Aditya Arora <10962267+arora-aditya@users.noreply.github.com>
2019-01-04 13:01:49 -0500

    added custom domain, fixed images, added captions, added custom font

--- assets/js/change_image.js
+++ assets/js/change_image.js
@@ -1,4 +1,16 @@
-const MAX_IMAGES = 22+1;
+const MAX_IMAGES = 9+1;
+const IMAGE_INFO = [
+  "Eres<br>Acrylic on canvas 5x5 ft",
+  "Euphoria<br>Serigraph on paper, 16 panels, each panel 18x18 in.",
+  "Etéreo<br>Oil on board, 16x16 in.",
+  "Elation<br>6 block, 3 color woodblock print on cotton wove paper, 50x46 in. Limited edition of 8, numbered and signed by the artist.",
+  "Disquiet<br>Serigraph on paper, 14 panels, each panel 18x18 in.",
+  "Window<br>Acrylic, epoxy resins, stainless steel, 24x18x2 in.",
+  "The Artificialist Manifesto<br>Inkjet prints on biaxially-oriented polyethylene terephthalate, stainless steel, steel fixtures, 11x8x0.5 in.",
+  "CMY book<br>Inkjet prints on biaxially-oriented polyethylene terephthalate, steel fixtures, 11x8x0.25 in.",
+  "One<br>Relief monotype print on cotton wove paper, 40x40 in.",
+  "Two<br>Relief monotype print on cotton wove paper, 40x40 in."
+];
 var current_IMAGE = 0;
 
 function prefetch(){
@@ -17,6 +29,7 @@ function prefetch(){
 function changeImage(){
   current_IMAGE = (current_IMAGE+1)%MAX_IMAGES;
   document.getElementById("changing").src="../assets/images/" + String(current_IMAGE) + ".jpg";
+  document.getElementById("caption").innerHTML = IMAGE_INFO[current_IMAGE];
 }
 
 function whitespaceClick(e){
