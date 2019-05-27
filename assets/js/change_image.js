bbb37b3be0201a94ec4cd0b44ed2e4f3cbe9c6a9
Aditya Arora <10962267+arora-aditya@users.noreply.github.com>
2019-05-27 00:56:47 -0400

    add new images and shift navbar above images - add 'alt' to all images, add lang='en' to all html files - update jquery to secure version

--- assets/js/change_image.js
+++ assets/js/change_image.js
@@ -1,15 +1,17 @@
-const MAX_IMAGES = 9+1;
+const MAX_IMAGES = 11+1;
 const IMAGE_INFO = [
-  "Eres<br>Acrylic on canvas 5x5 ft",
-  "Euphoria<br>Serigraph on paper, 16 panels, each panel 18x18 in.",
-  "Etéreo<br>Oil on board, 16x16 in.",
-  "Elation<br>6 block, 3 color woodblock print on cotton wove paper, 50x46 in. Limited edition of 8, numbered and signed by the artist.",
-  "Disquiet<br>Serigraph on paper, 14 panels, each panel 18x18 in.",
-  "Window<br>Acrylic, epoxy resins, stainless steel, 24x18x2 in.",
-  "The Artificialist Manifesto<br>Inkjet prints on biaxially-oriented polyethylene terephthalate, stainless steel, steel fixtures, 11x8x0.5 in.",
-  "CMY book<br>Inkjet prints on biaxially-oriented polyethylene terephthalate, steel fixtures, 11x8x0.25 in.",
-  "One<br>Relief monotype print on cotton wove paper, 40x40 in.",
-  "Two<br>Relief monotype print on cotton wove paper, 40x40 in."
+  "Deconstruction <br> Acrylic on canvas mounted on board, 5x5 ft.",
+  "Sisyphus <br> Acrylic on canvas mounted on board 50x50 in.",
+  "Eres <br> Acrylic on canvas 5x5 ft.",
+  "Euphoria <br> Serigraph on paper, 16 panels, each panel 18x18 in.",
+  "Etéreo <br> Oil on board, 16x16 in.",
+  "Elation <br> 6 block, 3 color woodblock print on cotton wove paper, 50x46 in. Limited edition of 8, numbered and signed by the artist.",
+  "Disquiet <br> Serigraph on paper, 14 panels, each panel 18x18 in.",
+  "Window <br> Acrylic, epoxy resins, stainless steel, 24x18x2 in.",
+  "The Artificialist Manifesto <br> Inkjet prints on biaxially-oriented polyethylene terephthalate, stainless steel, steel fixtures, 11x8x0.5 in.",
+  "CMY book <br> Inkjet prints on biaxially-oriented polyethylene terephthalate, steel fixtures, 11x8x0.25 in.",
+  "One <br> Relief monotype print on cotton wove paper, 40x40 in.",
+  "Two <br> Relief monotype print on cotton wove paper, 40x40 in."
 ];
 var current_IMAGE = -1;
 
@@ -23,6 +25,7 @@ async function prefetch(){
   for(var i = current_IMAGE+1; i < MAX_IMAGES; i+=1){
     var Image = document.createElement("img")
     Image.src = "../assets/images/" + String(i) + ".jpg";
+    Image.alt = IMAGE_INFO[i];
     // Image.className = "d-none"
     Image.width = 0;
     Image.height = 0;
@@ -36,6 +39,7 @@ async function prefetch(){
 function changeImage(){
   current_IMAGE = (current_IMAGE+1)%MAX_IMAGES;
   document.getElementById("changing").src="../assets/images/" + String(current_IMAGE) + ".jpg";
+  document.getElementById("changing").alt=IMAGE_INFO[current_IMAGE];
   document.getElementById("caption").innerHTML = IMAGE_INFO[current_IMAGE];
 }
 
